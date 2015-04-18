dc5497767b0884e8618301a90a9ae191951e2aa2
Anjelson <anjelson@gmail.com>
2015-04-17 19:39:09 -0500

    First Commit

--- src/main/java/com/anjelson/almightyswitch/AlmightySwitch.java
+++ src/main/java/com/anjelson/almightyswitch/AlmightySwitch.java
@@ -1,15 +1,21 @@
 package com.anjelson.almightyswitch;
 
+import com.anjelson.almightyswitch.proxy.IProxy;
+import com.anjelson.almightyswitch.reference.Reference;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid="AlmightySwitch", name="Almighty Switch", version="1.7.10-1.0")
+@Mod(modid= Reference.MOD_ID, name=Reference.MOD_NAME, version=Reference.VERSION)
 public class AlmightySwitch
 {
     @Mod.Instance("AlmightySwitch")
     public static AlmightySwitch instance;
+
+    @SidedProxy(clientSide = "com.anjelson.almightyswitch.proxy.ClientProxy", serverSide = "com.anjelson.almightyswitch.proxy.ServerProxy")
+    public static IProxy proxy;
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event)
---
+++ src/main/java/com/anjelson/almightyswitch/reference/Reference.java
@@ -0,0 +1,10 @@
+package com.anjelson.almightyswitch.reference;
+
+/*constants*/
+
+public class Reference
+{
+    public static final String MOD_ID = "AlmightySwitch";
+    public static final String MOD_NAME="Almighty Switch";
+    public static final String VERSION = "1.7.10-1.0";
+}
